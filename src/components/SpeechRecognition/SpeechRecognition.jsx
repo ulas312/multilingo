fd2f859cf60e052711795f701316883b5c128692
ulas312 <temel.ulas@googlemail.com>
2024-08-09 01:33:32 +0100

    working on speech recognition

---
+++ src/components/SpeechRecognition/SpeechRecognition.jsx
@@ -0,0 +1,33 @@
+import React, { useEffect } from 'react';
+import SpeechRecognition, {
+  useSpeechRecognition,
+} from 'react-speech-recognition';
+import { IconMicrophone } from '@tabler/icons-react';
+
+const SpeechRecognitionComponent = ({ setSourceText }) => {
+  const { transcript, listening } = useSpeechRecognition();
+
+  useEffect(() => {
+    setSourceText(transcript);
+  }, [transcript, setSourceText]);
+
+  const handlingVoiceRecording = () => {
+    if (listening) {
+      SpeechRecognition.stopListening();
+    } else {
+      SpeechRecognition.startListening();
+    }
+  };
+
+  return (
+    <div>
+      <IconMicrophone
+        size={22}
+        className='text-gray-400'
+        onClick={handlingVoiceRecording}
+      />
+    </div>
+  );
+};
+
+export default SpeechRecognitionComponent;
